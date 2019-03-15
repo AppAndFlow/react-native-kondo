0bf02181f5e1a38dd29b6e6078aa175e22b093d7
Samuel Chamberland <sam.chamberland@me.com>
2019-03-15 14:05:35 -0400

    Add <Box /> and <Text /> primitives

---
+++ src/Box.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { View } from 'react-native';
+
+const Box = props => <View>{props.children}</View>;
+
+export default Box;
---
+++ src/Text.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { Text as ReactNativeText } from 'react-native';
+
+const Text = props => <ReactNativeText>{props.children}</ReactNativeText>;
+
+export default Text;
