2025a1fe911cfa82ec3211979777b02f9462e9ca
Samuel Chamberland <sam.chamberland@me.com>
2019-03-15 13:58:45 -0400

    Add basic theme provider

--- src/ThemeProvider.tsx
+++ src/ThemeProvider.tsx
@@ -1,1 +1,22 @@
 import * as React from 'react';
+
+import theme, { Theme } from './theme';
+
+const ThemeContext = React.createContext({});
+
+interface Props {
+  children: (theme: Theme) => React.ReactNode;
+  theme: Theme;
+}
+
+const ThemeProvider = (props: Props) => (
+  <ThemeContext.Provider value={props.theme}>
+    {props.children(theme)}
+  </ThemeContext.Provider>
+);
+
+ThemeProvider.defaultProps = {
+  theme,
+};
+
+export default ThemeProvider;
---
+++ src/theme.ts
@@ -0,0 +1,25 @@
+export interface Theme {
+  borders: {};
+  colors: {};
+  fonts: {};
+  fontSizes: {};
+  letterSpacings: {};
+  lineHeights: {};
+  opacity: {};
+  shadows: {};
+  space: {};
+}
+
+const theme: Theme = {
+  borders: {},
+  colors: {},
+  fonts: {},
+  fontSizes: {},
+  letterSpacings: {},
+  lineHeights: {},
+  opacity: {},
+  shadows: {},
+  space: {},
+};
+
+export default theme;
