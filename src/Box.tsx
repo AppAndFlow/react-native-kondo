81c036ec5088c600100e0d9359f7f8c596f861d1
Samuel Chamberland <sam.chamberland@me.com>
2019-03-18 16:14:11 -0400

    Add basic docz pages

--- src/Box.tsx
+++ src/Box.tsx
@@ -1,27 +1,40 @@
 import * as React from 'react';
-import { View, ViewProps } from 'react-native';
+import { StyleSheet, View, ViewProps, ViewStyle } from 'react-native';
 
 interface Props {
-  bg: string;
+  bg?: string;
   flex?: number;
   height: number;
-  m: number;
-  mb: number;
-  ml: number;
-  mr: number;
-  mt: number;
-  mx: number;
-  my: number;
-  p: number;
-  pb: number;
-  pl: number;
-  pr: number;
-  pt: number;
-  px: number;
-  py: number;
-  width: number;
+  m?: number;
+  mb?: number;
+  ml?: number;
+  mr?: number;
+  mt?: number;
+  mx?: number;
+  my?: number;
+  p?: number;
+  pb?: number;
+  pl?: number;
+  pr?: number;
+  pt?: number;
+  px?: number;
+  py?: number;
+  width?: number;
 }
 
-const Box = (props: Props & ViewProps) => <View {...props} />;
+function getStyleSheetFromBoxProps(props: Props) {
+  const { height, width } = props;
+  const style: ViewStyle = {};
+
+  if (props.bg) {
+    style.backgroundColor = props.bg;
+  }
+
+  return StyleSheet.flatten([style, { height, width }]);
+}
+
+const Box = (props: Props & ViewProps) => (
+  <View style={{ ...getStyleSheetFromBoxProps(props) }} {...props} />
+);
 
 export { Box };
--- src/Text.tsx
+++ src/Text.tsx
@@ -1,6 +1,27 @@
 import * as React from 'react';
-import { Text as ReactNativeText, TextProps } from 'react-native';
+import {
+  StyleSheet,
+  Text as ReactNativeText,
+  TextProps,
+  TextStyle,
+} from 'react-native';
 
-const Text = (props: TextProps) => <ReactNativeText {...props} />;
+interface Props {
+  fontSize: number;
+}
+
+function getStyleSheetFromTextProps(props: Props) {
+  const { fontSize } = props;
+  const style: TextStyle = {};
+
+  return StyleSheet.flatten([style, { fontSize }]);
+}
+
+const Text = (props: Props & TextProps) => (
+  <ReactNativeText
+    style={{ ...getStyleSheetFromTextProps(props) }}
+    {...props}
+  />
+);
 
 export { Text };
