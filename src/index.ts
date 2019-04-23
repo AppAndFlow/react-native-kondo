0d6bf2fef633b4c2874f362e703d1229bb911021
Samuel Chamberland <sam.chamberland@me.com>
2019-04-23 15:46:56 -0400

    Export injected props type

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ export { default as Box, BoxProps } from './Box';
 export { default as Text, TextProps } from './Text';
 export { default as Touchable, TouchableProps, Feedback } from './Touchable';
 export { ThemeProvider, ThemeConsumer } from './ThemeProvider';
-export { default as withTheme } from './withTheme';
+export { default as withTheme, WithThemeInjectedProps } from './withTheme';
