d3390de3cce936935c01904612a5d84bed65d9de
Samuel Chamberland <sam.chamberland@me.com>
2019-04-23 14:55:07 -0400

    Add enum export to entry point

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { default as Box, BoxProps } from './Box';
 export { default as Text, TextProps } from './Text';
-export { default as Touchable, TouchableProps } from './Touchable';
+export { default as Touchable, TouchableProps, Feedback } from './Touchable';
 export { ThemeProvider, ThemeConsumer } from './ThemeProvider';
 export { default as withTheme } from './withTheme';
