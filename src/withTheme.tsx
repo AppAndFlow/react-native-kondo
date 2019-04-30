c7532a07cd5fa5916cbcde885e7b8d67ffa08d9d
Samuel Chamberland <sam.chamberland@me.com>
2019-04-30 14:14:21 -0400

    Ensure static properties are copied when using HOC

--- src/withTheme.tsx
+++ src/withTheme.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import hoistStatics from 'hoist-non-react-statics';
 
 import { Theme } from './theme';
 import { ThemeConsumer } from './ThemeProvider';
@@ -7,22 +8,25 @@ export interface ThemeInjectedProps {
   theme: Theme;
 }
 
-const withTheme = (WrappedComponent: React.ComponentType<any>) => {
-  return class extends React.Component {
-    static displayName = `withTheme(${WrappedComponent.displayName ||
-      WrappedComponent.name ||
-      'Component'})`;
+const withTheme = (WrappedComponent: any) => {
+  return hoistStatics(
+    class EnhancedComponent extends React.Component {
+      static displayName = `withTheme(${WrappedComponent.displayName ||
+        WrappedComponent.name ||
+        'Component'})`;
 
-    render() {
-      return (
-        <ThemeConsumer>
-          {(value: { theme: Theme }) => (
-            <WrappedComponent theme={value.theme} {...this.props} />
-          )}
-        </ThemeConsumer>
-      );
-    }
-  };
+      render() {
+        return (
+          <ThemeConsumer>
+            {(value: { theme: Theme }) => (
+              <WrappedComponent theme={value.theme} {...this.props} />
+            )}
+          </ThemeConsumer>
+        );
+      }
+    },
+    WrappedComponent,
+  );
 };
 
 export default withTheme;
